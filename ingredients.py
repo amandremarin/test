391832107f37be95219bb827fd059e14ca74af56
Andre Marin <amandremarin@users.noreply.github.com>
2018-08-11 08:28:41 -0400

    Refactored into functions and added xml file

--- ingredients.py
+++ ingredients.py
@@ -3,23 +3,33 @@ import re
 from collections import Counter
 import matplotlib.pyplot as plt
 
-tree = ET.parse(r'C:\Users\ANDRE\Downloads\cookbook_import_pages_current.xml\cookbook_import_pages_current.xml')
-root = tree.getroot()
+def root_from_xml_filename(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    return root
 
-all_ingredients = []
+def lists_all_ingredients_from_root(root):
 
-for text in root.findall('*/*/*'):
- if text.text: m = re.search('Ingredients.*Directions',text.text, re.DOTALL)
- if m: x = m.group()
- if m: y = re.findall(r'\[\[.*\]\]',x)
- if m and y: 
-  for ingredient in y:
-   all_ingredients.append(ingredient)
+    all_ingredients = []
 
-top_ingredients = Counter(all_ingredients)
-over_100 = {key: value for (key, value) in top_ingredients.items() if value > 100}
-x = [i for i in range(1, len(over_100)+1)]
-y = [x for x in over_100.values()]
-y.sort()
-plt.bar(x, y)
-plt.show()
+    for text in root.findall('*/*/*'):
+        if text.text: m = re.search('Ingredients.*Directions',text.text, re.DOTALL)
+        if m: x = m.group()
+        if m: y = re.findall(r'\[\[.*\]\]',x)
+        if m and y: 
+            for ingredient in y:
+                all_ingredients.append(ingredient)
+    return all_ingredients
+
+def plot_from_ingredient_count(ingredient_count):
+    x = [i for i in range(len(ingredient_count))]
+    y = [i for i in ingredient_count.values()]
+    y.sort(reverse=True)
+    plt.bar(x, y)
+    plt.show()
+
+root = root_from_xml_filename('cookbook_import_pages_current.xml')
+all_ingredients = lists_all_ingredients_from_root(root)
+count_ingredients = Counter(all_ingredients)
+over_100 = {key: value for (key, value) in count_ingredients.items() if value > 100}
+plot_from_ingredient_count(over_100)
