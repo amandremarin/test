8d2e67513875bf998ee4c695583b8ba77b379000
Andre Marin <amandremarin@users.noreply.github.com>
2018-08-11 07:24:23 -0400

    Graphed top 100 ingredients

--- ingredients.py
+++ ingredients.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import re
 from collections import Counter
+import matplotlib.pyplot as plt
 
 tree = ET.parse(r'C:\Users\ANDRE\Downloads\cookbook_import_pages_current.xml\cookbook_import_pages_current.xml')
 root = tree.getroot()
@@ -16,3 +17,9 @@ for text in root.findall('*/*/*'):
    all_ingredients.append(ingredient)
 
 top_ingredients = Counter(all_ingredients)
+over_100 = {key: value for (key, value) in top_ingredients.items() if value > 100}
+x = [i for i in range(1, len(over_100)+1)]
+y = [x for x in over_100.values()]
+y.sort()
+plt.bar(x, y)
+plt.show()
