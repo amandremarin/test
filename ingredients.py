3b6da28cc00d7c99ec2e6f5a8491f15dcb8d9d6f
Andre Marin <amandremarin@users.noreply.github.com>
2018-08-12 10:06:00 -0400

    horizontal graph, changed to top x, cleaned names

--- ingredients.py
+++ ingredients.py
@@ -3,15 +3,31 @@ import re
 from collections import Counter
 import matplotlib.pyplot as plt
 
+def plot_top(x_ingredients):
+    root = root_from_xml_filename('cookbook_import_pages_current.xml')
+    all_ingredients = list_all_ingredients_from_root(root)
+    ingredients_count = Counter(all_ingredients)
+    top_x_ingredients = {key: value for (key, value) in ingredients_count.most_common(x_ingredients)}
+    x = [i for i in range(len(top_x_ingredients))]
+    y = [i for i in top_x_ingredients.values()]
+    plt.barh(x, y, align='center')
+    labels = [re.sub('[\[\]]','', label) for label in top_x_ingredients.keys()]
+    plt.yticks(x, labels)
+    if x_ingredients <= 35:
+        pass
+    elif x_ingredients <= 50:
+        plt.tick_params(axis='y', labelsize=8)
+    else:
+        plt.tick_params(axis='y', labelsize=5)
+    plt.show()
+
 def root_from_xml_filename(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     return root
 
-def lists_all_ingredients_from_root(root):
-
+def list_all_ingredients_from_root(root):
     all_ingredients = []
-
     for text in root.findall('*/*/*'):
         if text.text: m = re.search('Ingredients.*Directions',text.text, re.DOTALL)
         if m: x = m.group()
@@ -21,21 +37,4 @@ def lists_all_ingredients_from_root(root):
                 all_ingredients.append(ingredient)
     return all_ingredients
 
-def plot_count_over(x_ingredients):
-    root = root_from_xml_filename('cookbook_import_pages_current.xml')
-    all_ingredients = lists_all_ingredients_from_root(root)
-    ingredients_count = Counter(all_ingredients)
-    over_x = {key: value for (key, value) in ingredients_count.items() if value > x_ingredients}
-    x = [i for i in range(len(over_x))]
-    y = [i for i in over_x.values()]
-    #y.sort(reverse=True)
-    plt.barh(x, y)
-    labels = [label for label in over_x.keys()]
-    plt.yticks(x, labels)
-    if x_ingredients >= 1000:
-        plt.tick_params(axis='y', labelsize=8)
-    elif x_ingredients >= 500:
-        plt.tick_params(axis='y', labelsize=5)
-    plt.show()
-
-plot_count_over(500)
+plot_top(75)
